7008ece41a49f1f746e97a655e7b1edb008aee50
JericoL <Jericolabajo11@gmail.com>
2024-06-15 16:56:57 +0800

    Add ClietController-DataTables-ViewModel

---
+++ PAWS-NDV-PetLovers/ViewModels/ClientVm.cs
@@ -0,0 +1,10 @@
+﻿using PAWS_NDV_PetLovers.Models.Records;
+
+namespace PAWS_NDV_PetLovers.ViewModels
+{
+    public class ClientVm
+    {
+        public Owner Owner { get; set; }
+        public Pet Pet { get; set; }
+    }
+}
---
+++ PAWS-NDV-PetLovers/wwwroot/js/clientCategory/Index.js
@@ -0,0 +1,3 @@
+﻿$(document).ready(function () {
+    var table = new DataTable('#ClientTable');
+})
