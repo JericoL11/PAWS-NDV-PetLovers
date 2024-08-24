88b45d686781b802ac706d58886745f8b522abdd
JericoL <Jericolabajo11@gmail.com>
2024-08-24 18:14:35 +0800

    Fixed Bugs - Added features on Services and Diagnosis

---
+++ PAWS-NDV-PetLovers/Migrations/20240824084223_addColumn_ServiceType.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PAWS_NDV_PetLovers.Migrations
+{
+    /// <inheritdoc />
+    public partial class addColumn_ServiceType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "serviceType",
+                table: "Services",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "serviceType",
+                table: "Services");
+        }
+    }
+}
