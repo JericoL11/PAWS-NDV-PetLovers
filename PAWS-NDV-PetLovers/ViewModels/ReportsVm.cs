e73d0548bbde1ad9abe18e90a5b8e4441c323b45
JericoL <Jericolabajo11@gmail.com>
2024-10-23 23:50:09 +0800

    Transaction Summary - Done

--- PAWS-NDV-PetLovers/ViewModels/ReportsVm.cs
+++ PAWS-NDV-PetLovers/ViewModels/ReportsVm.cs
@@ -1,5 +1,6 @@
 ﻿using PAWS_NDV_PetLovers.Models.Appointments;
 using PAWS_NDV_PetLovers.Models.Records;
+using PAWS_NDV_PetLovers.Models.Transactions;
 
 namespace PAWS_NDV_PetLovers.ViewModels
 {
@@ -33,12 +34,13 @@ namespace PAWS_NDV_PetLovers.ViewModels
 
         public IEnumerable <StockAdjustment> IstockAdjustment { get; set; }
 
+        public IEnumerable<Billing>IBilling { get; set; }
 
-        public ProdMgmtTab activeProdMgmtTab { get; set; }
+        public ReportTab activeReportTab { get; set; }
     }
 
 
-    public enum ProdMgmtTab
+    public enum ReportTab
     {
         stockAdjust,
         stockLevel,
